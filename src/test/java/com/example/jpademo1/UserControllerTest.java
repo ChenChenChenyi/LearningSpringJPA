a2bbf3ecc58069ffadfa84a2d8c7f36fc98252a5
chenyi <614362234@qq.com>
2021-02-01 11:20:14 +0800

    添加测试类

---
+++ src/test/java/com/example/jpademo1/UserControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.jpademo1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Auther: 陈义
+ * @Date: 2021/2/1 11:09
+ * @Description:
+ */
+class UserControllerTest {
+
+    @Test
+    void addNewUser() {
+    }
+
+    @Test
+    void getAllUser() {
+    }
+
+    @Test
+    void getAllUsersWithSort() {
+    }
+
+    @Test
+    void getUserByPage() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findByFirstName() {
+    }
+}
